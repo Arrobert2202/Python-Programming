685054dfb29aac8ebeea618e0687b401b94ea633
rubyrobert2002 <robertaron15@yahoo.ro>
2023-10-17 16:55:57 +0300

    tema1

--- Lab2/ex5.py
+++ Lab2/ex5.py
@@ -3,6 +3,7 @@ def nth_root(a, n):
   x_0 = a // 2
   for _ in range(2000):
     x_0 = (x_0 * (n - 1) // n) + (a // n) // (x_0 ** (n-1))
-  print(x_0 / (10 ** 50))
+  result = x_0 / (10 ** 50)
+  print(f"{result:.50f}")
 
-nth_root(20,4)
+nth_root(32,3)
